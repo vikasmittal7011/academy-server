fe348fea2122f3d3bab64ae03ef8e2bcc95dd1b2
vikasmittal7011 <vikasaggrawal700@gmail.com>
2024-03-15 12:13:06 +0530

    Modify enroll course method

--- controller/CourseEnroll.js
+++ controller/CourseEnroll.js
@@ -1,19 +1,38 @@
 import HttpError from "../models/http-error.js";
-import { CourseEnroll } from "../models/CourseEnroll.js"
+import { CourseEnroll } from "../models/CourseEnroll.js";
+import crypto from "crypto";
 
 const success = true;
 
 export const createCourseEnroll = async (req, res, next) => {
   try {
 
-    const createCourseEnroll = { ...req.body };
-    let courseEnroll = await CourseEnroll.create(createCourseEnroll);
+    const {
+      razorpay_payment_id,
+      razorpay_order_id,
+      razorpay_signature
+    } = req.body
 
-    if (!courseEnroll) {
-      return next(new HttpError("CourseEnroll not add , Plase try again later"));
+    const body = razorpay_order_id + "|" + razorpay_payment_id
+
+    const expectedsignature = crypto.createHmac("sha256", process.env.RAZOR_PAY_SECRET).update(body.toString()).digest("hex")
+
+    const isAuth = expectedsignature === razorpay_signature
+
+    if (isAuth) {
+
+      const createCourseEnroll = { ...req.query, dateOfEnroll: new Date(), user: req.userData.id };
+      let courseEnroll = await CourseEnroll.create(createCourseEnroll);
+
+      if (!courseEnroll) {
+        return res.redirect("http://localhost:3000/failer/" + "Booking can't be done, plase try again lagter!!")
+      }
+
+      return res.redirect("http://localhost:3000/confirm/" + courseEnroll.id)
+
+    } else {
+      return res.redirect("http://localhost:3000/failer/" + "Booking can't be done, plase try again lagter!!")
     }
-
-    res.json({ success, courseEnroll })
 
   } catch (err) {
     return next(new HttpError(err.message, 500));
